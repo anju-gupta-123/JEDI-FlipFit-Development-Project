84942aa5090968cca0ebbeeec4cae892855fa33e
aaryan-shetty-flipkart <aaryan.shetty@flipkart.com>
2025-01-22 14:21:43 +0530

    Added admin operations code

--- JEDI-GroupC-FlipFit-POS/src/com/flipkart/bean/Admin.java
+++ JEDI-GroupC-FlipFit-POS/src/com/flipkart/bean/Admin.java
@@ -1,6 +1,40 @@
 package com.flipkart.bean;
+import java.util.*;
 
 public class Admin extends User{
+	static List<Gym_Center> gymCenters = new ArrayList<>();
+	static List<Gym_Center> approvedGymCenters = new ArrayList<>();
+	/**
+	 * @return the gymCenters
+	 */
+	public static List<Gym_Center> getGymCenters() {
+		return gymCenters;
+	}
+	
+	public static void removeGymCenters(Gym_Center gymcenter) {
+		Admin.gymCenters.remove(gymcenter);
+	}
+	/**
+	 * @param gymCenters the gymCenters to set
+	 */
+	public static void setGymCenters(Gym_Center gymcenter) {
+		Admin.gymCenters.add(gymcenter);
+	}
+	/**
+	 * @return the approvedGymCenters
+	 */
+	public static List<Gym_Center> getApprovedGymCenters() {
+		return approvedGymCenters;
+	}
+	/**
+	 * @param approvedGymCenters the approvedGymCenters to set
+	 */
+	public static void setApprovedGymCenters(Gym_Center gymcenter) {
+		Admin.gymCenters.add(gymcenter);
+	}
+	
+	
+	
 	
 
 }
