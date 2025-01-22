6f2366a74f585c93b3455e083b8347d1917cc1c0
DhruvFlip20 <dhruv.gupta2@flipkart.com>
2025-01-22 15:33:43 +0530

    gym owner changes

--- JEDI-GroupC-FlipFit-POS/src/com/flipkart/business/GymOwnerOperations.java
+++ JEDI-GroupC-FlipFit-POS/src/com/flipkart/business/GymOwnerOperations.java
@@ -2,8 +2,6 @@ package com.flipkart.business;
 
 import com.flipkart.bean.Gym_Center;
 import com.flipkart.bean.Customer;
-import com.flipkart.bean.User;
-import com.flipkart.bean.Admin;
 import com.flipkart.bean.Gym_Owner;
 
 import java.util.*;
@@ -12,20 +10,7 @@ public class GymOwnerOperations {
     public List<Gym_Center> gymCenters = new ArrayList<>();
     public Map<Integer, List<Customer>> registeredCustomers = new HashMap<>();
     public List<Gym_Center> approvedgymCenters = new ArrayList<>();
-    public void registerGymOwner() {
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println("Enter GymOwner ID:");
-        int id = sc.nextInt();
-        sc.nextLine(); 
-        System.out.println("Enter GymOwner Name:");
-        String name = sc.nextLine();
-        System.out.println("Enter Contact Number:");
-        String contact=sc.nextLine();
-        System.out.println("Enter Email Address:");
-        String email=sc.nextLine();
-        System.out.println("Enter Password:");
-        String password=sc.nextLine();
-        sc.close();
+    public void registerGymOwner(int id, String contact, String email, String name, String password) {
         Gym_Owner gymowner  = new Gym_Owner();
         gymowner.setId(id);
         gymowner.setContact(contact);
@@ -34,22 +19,7 @@ public class GymOwnerOperations {
         gymowner.setPassword(password);
         
     }
-    public void registerGym() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Gym Center ID:");
-        int centerId = sc.nextInt();
-        sc.nextLine(); 
-        System.out.println("Enter Owner ID:");
-        int ownerId = sc.nextInt();
-        sc.nextLine(); 
-        System.out.println("Enter Gym Center Name:");
-        String centerName = sc.nextLine();
-        System.out.println("Enter Address:");
-        String address = sc.nextLine();
-        System.out.println("Enter Number of Slots:");
-        int noOfSlots = sc.nextInt();
-        sc.close();
+    public void registerGym(int centerId, int ownerId, int noOfSlots, String centerName, String address) {
         Gym_Center gym = new Gym_Center();
         gym.setCenter_id(centerId);
         gym.setOwner_id(ownerId);
@@ -57,26 +27,18 @@ public class GymOwnerOperations {
         gym.setAddress(address);
         gym.setNo_of_slots(noOfSlots);
         
-/       Admin.setGym_Center(gym);
+//       Admin.setGym_Center(gym);
        
         
         System.out.println("Gym registered successfully!");
     }
 
-    public void addSlots() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Gym Center ID to add slots:");
-        int centerId = sc.nextInt();
+    public void addSlots(int centerId, int additionalSlots) {
 
         Optional<Gym_Center> gymOptional = gymCenters.stream()
                 .filter(g -> g.getCenter_id() == centerId)
                 .findFirst();
-        sc.close();
         if (gymOptional.isPresent()) {
-            System.out.println("Enter number of slots to add:");
-            int additionalSlots = sc.nextInt();
-
             Gym_Center gym = gymOptional.get();
             gym.setNo_of_slots(gym.getNo_of_slots() + additionalSlots);
 
@@ -86,34 +48,19 @@ public class GymOwnerOperations {
         }
     }
 
-    public void editGymDetails() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Gym Center ID to edit details:");
-        int centerId = sc.nextInt();
-        sc.nextLine(); 
-        sc.close();
+    public void editGymDetails(int centerId, String centerName, String address, String slotsInput) {
         Optional<Gym_Center> gymOptional = gymCenters.stream()
                 .filter(g -> g.getCenter_id() == centerId)
                 .findFirst();
 
         if (gymOptional.isPresent()) {
             Gym_Center gym = gymOptional.get();
-
-            System.out.println("Enter new Gym Center Name :");
-            String centerName = sc.nextLine();
             if (!centerName.isEmpty()) {
                 gym.setCenter_name(centerName);
             }
-
-            System.out.println("Enter new Address :");
-            String address = sc.nextLine();
             if (!address.isEmpty()) {
                 gym.setAddress(address);
             }
-
-            System.out.println("Enter new Number of Slots :");
-            String slotsInput = sc.nextLine();
             if (!slotsInput.isEmpty()) {
                 int noOfSlots = Integer.parseInt(slotsInput);
                 gym.setNo_of_slots(noOfSlots);
@@ -125,30 +72,12 @@ public class GymOwnerOperations {
         }
     }
 
-    public void registerCustomer() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Gym Center ID for customer registration:");
-        int centerId = sc.nextInt();
-        sc.nextLine(); 
-        sc.close();
+    public void registerCustomer(int centerId, int id, String name, String contact, String email, String password) {
         Optional<Gym_Center> gymOptional = gymCenters.stream()
                 .filter(g -> g.getCenter_id() == centerId)
                 .findFirst();
 
         if (gymOptional.isPresent()) {
-            System.out.println("Enter Customer ID:");
-            int id = sc.nextInt();
-            sc.nextLine(); 
-            System.out.println("Enter Customer Name:");
-            String name = sc.nextLine();
-            System.out.println("Enter Contact Number:");
-            String contact=sc.nextLine();
-            System.out.println("Enter Email Address:");
-            String email=sc.nextLine();
-            System.out.println("Enter Password:");
-            String password=sc.nextLine();
-            sc.close();
             Customer customer = new Customer();
             customer.setId(id);
             customer.setContact(contact);
