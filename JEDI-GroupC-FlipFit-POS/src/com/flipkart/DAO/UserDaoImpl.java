5f911de4cf92b3302b588a7eefd5faea81673c1b
aaryan-shetty-flipkart <aaryan.shetty@flipkart.com>
2025-01-24 14:45:55 +0530

    Fixed issues in customer operations and dao

--- JEDI-GroupC-FlipFit-POS/src/com/flipkart/DAO/UserDaoImpl.java
+++ JEDI-GroupC-FlipFit-POS/src/com/flipkart/DAO/UserDaoImpl.java
@@ -37,12 +37,13 @@ public class UserDaoImpl implements UserDAOInterface {
 		try {  
 			
 			Connection con = DBUtils.connect();
-			PreparedStatement stmt=con.prepareStatement("insert into flipfitGymOwner (customerName, email, contact, customerPassword) values(?,?,?,?)");  
+			PreparedStatement stmt=con.prepareStatement("insert into flipfitGymOwner (gymOwnerName, email, contact, gymOwnerPassword,aadharCard, gstNo) values(?,?,?,?,?,?)");  
 			stmt.setString(1, gymowner.getName()); 
 			stmt.setString(2, gymowner.getEmail());
 			stmt.setString(3, gymowner.getContact());
 			stmt.setString(4, gymowner.getPassword());
-			  
+			stmt.setString(5, gymowner.getAadharCard());
+			stmt.setString(6, gymowner.getGSTNo());
 			int i=stmt.executeUpdate();  
 			System.out.println(i+" records inserted");  
 			con.close(); 
@@ -61,16 +62,6 @@ public class UserDaoImpl implements UserDAOInterface {
 		return false;
 	}
 
-	public static void main(String args[]) {
-		
-		Customer customer = new Customer();
-		customer.setName("Aayush");
-		customer.setEmail("royaayush2002@gmail.com");
-		customer.setContact("9450662588");
-		customer.setPassword("hello@123");
-		
-		UserDaoImpl uDaoImpl = new UserDaoImpl();
-		uDaoImpl.registerCustomer(customer);
-	}
+	
 
 }
